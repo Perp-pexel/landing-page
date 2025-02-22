7a0d8ede399b50e55ddd7fe98724b5c5603fd31a
Perp-pexel <perpaman44@gmail.com>
2025-02-22 19:51:04 +0000

    hero

--- script.js
+++ script.js
@@ -33,66 +33,66 @@ function prev() {
     showSlide(currentIndex);
 }
 
-// Auto-slide every 5 seconds
-setInterval(next, 7000);
+// Auto-slide every 6 seconds
+setInterval(next, 6000);
 
 // Initialize first slide
 showSlide(currentIndex);
 
 
-// Typewrie
-class Typewriter {
-    constructor(element, words, period) {
-        this.words = words;
-        this.element = element;
-        this.loopNum = 0;
-        this.period = parseInt(period, 10) || 2000;
-        this.txt = "";
-        this.isDeleting = false;
-        this.tick();
-    }
+// // Typewrite
+// class Typewriter {
+//     constructor(element, words, period) {
+//         this.words = words;
+//         this.element = element;
+//         this.loopNum = 0;
+//         this.period = parseInt(period, 10) || 2000;
+//         this.txt = "";
+//         this.isDeleting = false;
+//         this.tick();
+//     }
 
-    tick() {
-        let i = this.loopNum % this.words.length;
-        let fullTxt = this.words[i];
+//     tick() {
+//         let i = this.loopNum % this.words.length;
+//         let fullTxt = this.words[i];
 
-        if (this.isDeleting) {
-            this.txt = fullTxt.substring(0, this.txt.length - 1);
-        } else {
-            this.txt = fullTxt.substring(0, this.txt.length + 1);
-        }
+//         if (this.isDeleting) {
+//             this.txt = fullTxt.substring(0, this.txt.length - 1);
+//         } else {
+//             this.txt = fullTxt.substring(0, this.txt.length + 1);
+//         }
 
-        this.element.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+//         this.element.innerHTML = '<span class="wrap">' + this.txt + "</span>";
 
-        let delta = 200 - Math.random() * 100;
+//         let delta = 200 - Math.random() * 100;
 
-        if (this.isDeleting) {
-            delta /= 2;
-        }
+//         if (this.isDeleting) {
+//             delta /= 2;
+//         }
 
-        if (!this.isDeleting && this.txt === fullTxt) {
-            delta = this.period;
-            this.isDeleting = true;
-        } else if (this.isDeleting && this.txt === "") {
-            this.isDeleting = false;
-            this.loopNum++;
-            delta = 500;
-        }
+//         if (!this.isDeleting && this.txt === fullTxt) {
+//             delta = this.period;
+//             this.isDeleting = true;
+//         } else if (this.isDeleting && this.txt === "") {
+//             this.isDeleting = false;
+//             this.loopNum++;
+//             delta = 500;
+//         }
 
-        setTimeout(() => this.tick(), delta);
-    }
-}
+//         setTimeout(() => this.tick(), delta);
+//     }
+// }
 
-document.addEventListener("DOMContentLoaded", function () {
-    let elements = document.getElementsByClassName("typewrite");
-    for (let i = 0; i < elements.length; i++) {
-        let toRotate = elements[i].getAttribute("data-type");
-        let period = elements[i].getAttribute("data-period");
+// document.addEventListener("DOMContentLoaded", function () {
+//     let elements = document.getElementsByClassName("typewrite");
+//     for (let i = 0; i < elements.length; i++) {
+//         let toRotate = elements[i].getAttribute("data-type");
+//         let period = elements[i].getAttribute("data-period");
 
-        if (toRotate) {
-            new Typewriter(elements[i], JSON.parse(toRotate), period);
-        }
-    }
-});
+//         if (toRotate) {
+//             new Typewriter(elements[i], JSON.parse(toRotate), period);
+//         }
+//     }
+// });
 
 
